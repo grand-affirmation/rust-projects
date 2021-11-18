fcac5d2c627f59943f6d8d94d9039a14c095096f
grand-affirmation <github@dta7.anonaddy.com>
2021-11-18 12:49:31 +0800

    program can now add new todos and write it to file

--- todo-manager/target/debug/deps/home-a678aadba044a88e.d
+++
@@ -1,7 +0,0 @@
-/home/ryan/dev/rust-projects/todo-manager/target/debug/deps/home-a678aadba044a88e.rmeta: /home/ryan/.cargo/registry/src/mirrors.ustc.edu.cn-12df342d903acd47/home-0.5.3/src/lib.rs
-
-/home/ryan/dev/rust-projects/todo-manager/target/debug/deps/libhome-a678aadba044a88e.rlib: /home/ryan/.cargo/registry/src/mirrors.ustc.edu.cn-12df342d903acd47/home-0.5.3/src/lib.rs
-
-/home/ryan/dev/rust-projects/todo-manager/target/debug/deps/home-a678aadba044a88e.d: /home/ryan/.cargo/registry/src/mirrors.ustc.edu.cn-12df342d903acd47/home-0.5.3/src/lib.rs
-
-/home/ryan/.cargo/registry/src/mirrors.ustc.edu.cn-12df342d903acd47/home-0.5.3/src/lib.rs:
--- todo-manager/target/debug/tm.d
+++
@@ -1,1 +0,0 @@
-/home/ryan/dev/rust-projects/todo-manager/target/debug/tm: /home/ryan/dev/rust-projects/todo-manager/src/lib.rs /home/ryan/dev/rust-projects/todo-manager/src/main.rs
