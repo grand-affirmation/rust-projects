b292562602aa1c9d6250ed2e29fafdc3cd87414c
grand-affirmation <github@dta7.anonaddy.com>
2021-11-28 15:05:05 +0800

    Password Generator can now parse arguments

--- 02 - password-generator/src/main.rs
+++ 02 - password-generator/src/main.rs
@@ -1,3 +1,12 @@
+use std::env::args;
+use std::process;
+use pg::PG;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().skip(1).collect();
+    let pg = PG::new(args);
+    if let Err(e) = pg {
+        eprintln!("Error: {}", e);
+        process::exit(1);
+    }
 }
